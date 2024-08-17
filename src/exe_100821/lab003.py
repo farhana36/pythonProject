1973a065a15bc5379a091c3536e6130ebb11d99a
Farhana <farhana36@gmail.com>
2024-08-17 23:01:19 +0530

    all files

---
+++ src/exe_100821/lab003.py
@@ -0,0 +1,16 @@
+print("Hello")
+#age = 51
+#print(age)
+#age = 45
+#pi = 3.14
+#print(type(pi))
+#a=10
+##b=10
+#c=10
+#sum=a+b+c
+#print(sum)
+#print(sum) ==sum+1
+#print(sum) == sum-1
+
+a,b,c = 45,65,12
+print(a,b,c,sep=("_"))
